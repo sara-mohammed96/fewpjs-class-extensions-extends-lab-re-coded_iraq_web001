9e49cd37e353353b0c821277150d05e6e4580bab
sara altaee <sara.korea96@gmail.com>
2020-06-16 18:04:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Your code here
+  
+class Polygon {
+  constructor(array){
+    this.sides = array;
+  }
+  get countSides() {
+    return this.sides.length
+  }
+  
+ get perimeter(){
+  let summation =this.sides.reduce((sum, side) => sum = sum+side, 0)
+  return summation
+  }
+}
+
+class Triangle extends Polygon{
+  isValid(){
+    let valid=true;
+   
+      if(this.sides[0]+this.sides[1]<=this.sides[2])
+    return  valid=false;
+      else
+    return  valid=true;
+   
+    
+  }
+  
+}
