45260171bec50f6c2a489765b0ce71797e1aeb14
sara altaee <sara.korea96@gmail.com>
2020-06-16 18:14:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,19 +14,17 @@ class Polygon {
 }
 
 class Triangle extends Polygon{
-  isValid(){
-    let valid=true;
-   for(let i=0;i<3;i++)
-     { if(this.sides[0]+this.sides[1]<=this.sides[2])
-    {  valid=false;
-      break;
-    }
+    get isValid(){
+    let valid = true;
     
+    for (let i = 0; i < 3; i++){
+      if(this.sides[0] + this.sides[1] <= this.sides[2]){
+        valid = false;
+        break;
+      }
       this.sides.unshift(this.sides.pop())
-      valid=true;
+    }
+    return valid
   }
-  return vaild;
-    
-  }
-  
 }
+    
